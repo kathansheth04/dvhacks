a32dd980082ff44adda4af16d9fd6a53393db8b1
kathansheth04 <sheth.kathan04@gmail.com>
2021-03-20 10:53:02 -0700

    splash screen with loading circle (need to route it)

---
+++ lib/screens/splash.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Splash());
+}
+
+class Splash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Splash',
+      debugShowCheckedModeBanner: false,
+      home: splash(),
+    );
+  }
+}
+
+class splash extends StatefulWidget {
+  splash({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  Body createState() => Body();
+}
+
+class MyCustomRoute<T> extends MaterialPageRoute<T> {
+  MyCustomRoute({WidgetBuilder builder, RouteSettings settings})
+      : super(builder: builder, settings: settings);
+
+  @override
+  Widget buildTransitions(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation, Widget child) {
+    // Fades between routes. (If you don't want any animation,
+    // just return child.)
+    return SlideTransition(
+      position: Tween<Offset>(
+        begin: const Offset(0.0, 0.0),
+        end: Offset.zero,
+      ).animate(animation),
+      child: child,
+    );
+  }
+}
+
+class Body extends State<splash> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    // This size provide us total height and width of our screen
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('lib/assets/welcomebg.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(height: size.height * 0.4),
+                new Container(
+                  child: CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation(const Color(0xFf66D5C1)),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
