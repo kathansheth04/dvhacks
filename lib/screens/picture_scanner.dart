7b6f518b48009c97b8bc0de70f545641594cffa0
Sashv Dave <davesashv@gmail.com>
2021-03-21 01:06:28 -0700

    Clean up image upload for algo refinements

--- lib/screens/picture_scanner.dart
+++ lib/screens/picture_scanner.dart
@@ -1,13 +1,8 @@
 import 'dart:io';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:firebase_ml_vision/firebase_ml_vision.dart';
-import 'package:dvhacks/algorithm.dart';
-import 'package:dvhacks/screens/dashboard.dart';
 import 'package:dvhacks/screens/splash.dart';
 import 'package:dvhacks/screens/mldetection.dart';
 
@@ -29,96 +24,15 @@ class DetectionScreen extends StatefulWidget {
 class _DetectionScreenState extends State<DetectionScreen> {
   File pickedImage;
 
-  bool isImageLoaded = false;
+  bool imageUploaded = false;
 
-  Future pickImage() async {
+  Future uploadImage() async {
     var tempStore = await ImagePicker().getImage(source: ImageSource.gallery);
 
     setState(() {
       pickedImage = File(tempStore.path);
-      isImageLoaded = true;
+      imageUploaded = true;
     });
-  }
-
-  CollectionReference firebase = Firestore.instance.collection("users");
-  final FirebaseAuth auth = FirebaseAuth.instance;
-  List<double> existing = new List<double>();
-  List<double> vals = new List<double>();
-
-  var approvalResult = "";
-  Future<void> evaluateFood() async {
-    final FirebaseUser user = await auth.currentUser();
-    final uid = user.uid;
-    FirebaseVisionImage ourImage = FirebaseVisionImage.fromFile(pickedImage);
-    TextRecognizer recognizeText = FirebaseVision.instance.textRecognizer();
-    VisionText readText = await recognizeText.processImage(ourImage);
-    List<String> variables = new List<String>();
-    List<double> variableValues = new List<double>();
-    for (TextBlock block in readText.blocks) {
-      for (TextLine line in block.lines) {
-        if (line.text.toLowerCase().contains("crude") ||
-            line.text.toLowerCase().contains("moisture") ||
-            line.text.toLowerCase().contains("fat") ||
-            line.text.toLowerCase().contains("fiber") ||
-            line.text.toLowerCase().contains("protein")) {
-          variables.add(line.text);
-        } else if (line.text.toLowerCase().contains("%")) {
-          final intValue = int.parse(
-              line.text.toLowerCase().replaceAll(RegExp('[^0-9]'), ''));
-          double floatValues = intValue / 10.0;
-          variableValues.add(floatValues);
-        }
-      }
-    }
-    print("Variables: ");
-    print(variables);
-    print("");
-    print("values");
-    print(variableValues);
-    /*
-     * Order of arrays: [protein, fat, fiber, moisture] 
-     */
-    return firebase
-        .document(uid.toString())
-        .get()
-        .then((DocumentSnapshot snapshot) async {
-      existing.add(double.parse(snapshot.data["protein"].toString()));
-      existing.add(double.parse(snapshot.data["fat"].toString()));
-      existing.add(double.parse(snapshot.data["fiber"].toString()));
-      print("existing: " + existing.toString());
-      vals = variableValues;
-      approvalResult = purchaseDecision(vals, existing, "dog");
-      print(approvalResult);
-    });
-  }
-
-  Future<void> readText() async {
-    FirebaseVisionImage ourImage = FirebaseVisionImage.fromFile(pickedImage);
-    TextRecognizer recognizeText = FirebaseVision.instance.textRecognizer();
-    VisionText readText = await recognizeText.processImage(ourImage);
-    List<String> variables = new List<String>();
-    List<double> variableValues = new List<double>();
-    for (TextBlock block in readText.blocks) {
-      for (TextLine line in block.lines) {
-        if (line.text.toLowerCase().contains("crude") ||
-            line.text.toLowerCase().contains("moisture") ||
-            line.text.toLowerCase().contains("fat") ||
-            line.text.toLowerCase().contains("fiber") ||
-            line.text.toLowerCase().contains("protein")) {
-          variables.add(line.text);
-        } else if (line.text.toLowerCase().contains("%")) {
-          final intValue = int.parse(
-              line.text.toLowerCase().replaceAll(RegExp('[^0-9]'), ''));
-          double floatValues = intValue / 10.0;
-          variableValues.add(floatValues);
-        }
-      }
-    }
-    print("Variables: ");
-    print(variables);
-    print("");
-    print("values");
-    print(variableValues);
   }
 
   @override
@@ -162,7 +76,7 @@ class _DetectionScreenState extends State<DetectionScreen> {
             ),
           ),
           SizedBox(height: 100.0),
-          isImageLoaded
+          imageUploaded
               ? Center(
                   child: Container(
                       height: 200.0,
@@ -223,14 +137,7 @@ class _DetectionScreenState extends State<DetectionScreen> {
             width: MediaQuery.of(context).size.width * 0.7,
             child: RaisedButton.icon(
                 icon: Icon(Icons.check_box),
-                onPressed: (() => {
-                      evaluateFood().then((value) => {
-                            if (approvalResult.toLowerCase() == "approve")
-                              {}
-                            else
-                              {}
-                          })
-                    }),
+                onPressed: () {},
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.all(Radius.circular(30.0))),
                 label: Text('Evaluate',
@@ -248,7 +155,7 @@ class _DetectionScreenState extends State<DetectionScreen> {
             width: MediaQuery.of(context).size.width * 0.7,
             child: RaisedButton.icon(
                 icon: Icon(Icons.cloud_download),
-                onPressed: pickImage,
+                onPressed: uploadImage,
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.all(Radius.circular(30.0))),
                 label: Text('Upload',
